70f211185ef8997e0ef4b559abacf991035c5b9b
Aro Militosyan <aro.militosyan@gmail.com>
2023-05-22 11:57:25 +0400

    Add files via upload

---
+++ routes/users.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+router.post('/', async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find(req.body);
+    res.send(users);
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).send({ error: 'User not found' });
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) {
+      return res.status(404).send({ error: 'User not found' });
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).send({ error: 'User not found' });
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+});
+
+router.delete('/', async (req, res) => {
+    try {
+      await User.deleteMany({});
+      res.send({ message: 'All users deleted' });
+    } catch (error) {
+      res.status(500).send({ error: error.message });
+    }
+  });
+  
+  module.exports = router;
